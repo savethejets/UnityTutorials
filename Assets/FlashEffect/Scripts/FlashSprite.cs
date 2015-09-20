08131c3507762e5f32019a4ae257cd1e8734e81c
Kyle Reczek <savethejets@hotmail.com>
2015-09-20 10:16:36 -0600

    Flash Effect tutorial

---
+++ Assets/FlashEffect/Scripts/FlashSprite.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashSprite : MonoBehaviour {
+
+	private SpriteRenderer _spriteRenderer;
+
+	void Start () {
+		_spriteRenderer = GetComponent<SpriteRenderer> ();
+		_spriteRenderer.material.SetFloat ("_FlashAmount", 1);
+	}
+
+	void Update () {
+
+	}
+}
