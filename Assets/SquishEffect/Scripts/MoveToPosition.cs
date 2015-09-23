de45b9ebb9e51cc82ca517f70367efb894d8ccec
Kyle Reczek <savethejets@hotmail.com>
2015-09-23 08:34:09 -0600

    Squish Effects initial Commit

---
+++ Assets/SquishEffect/Scripts/MoveToPosition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToPosition : MonoBehaviour {
+
+	public Vector2 Velocity;
+	public float Speed;
+	public float MaxSpeed;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Velocity = Vector2.up * 40 * Speed * Time.fixedDeltaTime;
+	}
+
+	public void FixedUpdate() {
+
+		var vector = -Vector2.up;
+
+		vector *= Speed * Time.fixedDeltaTime;
+
+		Velocity += vector;
+
+		Velocity = new Vector2 (Mathf.Clamp (Velocity.x, -MaxSpeed, MaxSpeed), Mathf.Clamp (Velocity.y, -MaxSpeed, 30f));
+
+		transform.Translate (Velocity);
+	}
+}
